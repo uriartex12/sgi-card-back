8e13ed94b289e62dea6847b83c86c3ce570cd74b
uriartex12 <prietomatex_1998@hotmail.com>
2025-01-08 16:39:28 -0500

    add constants url components

--- src/main/java/com/sgi/card/application/service/CardServiceImpl.java
+++ src/main/java/com/sgi/card/application/service/CardServiceImpl.java
@@ -12,8 +12,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
